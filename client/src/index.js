afd8581c6dd61bd173ffe5e6f91c0a025e0a5af7
hwrdyen <hwrdpvt@gmail.com>
2023-12-26 23:19:15 -0500

    Added redux actions and reducers -- resources are outdated

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { StylesProvider } from "@mui/styles";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose } from "redux";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { thunk } from "redux-thunk";
+
+import reducers from "./reducers";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore({
+  reducers: reducers,
+  middleware: [...getDefaultMiddleware(), thunk],
+});
+
+// const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <StylesProvider store={store}>
+    <Provider>
+      <App />
+    </Provider>
+  </StylesProvider>,
+  document.getElementById("root")
+);
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
+// where the routes are
+
 import express from "express";
+import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("THIS WORKS");
-});
+router.get("/", getPosts);
 
 export default router;
